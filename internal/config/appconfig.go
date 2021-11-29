7ad080c85fefb44d962dfc8f43a37f3716ffd86b
Daniel S <standeo@gmail.com>
2021-11-29 20:56:32 +1100

    Init root command using cobra

---
+++ internal/config/appconfig.go
@@ -0,0 +1,7 @@
+package config
+
+type AppConfig struct{}
+
+func NewConfig() (*AppConfig, error) {
+	return nil, nil
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/danstn/moped/cmd"
+
+func main() {
+	cmd.Execute()
+}
